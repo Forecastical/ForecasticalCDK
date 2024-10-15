91071763b2c4251c6bec96105b22d1ac8cecce3c
esteban1324 <estebanlinarez@ymail.com>
2024-10-15 15:36:23 -0400

    mapcard has map, need reformatting and overlay tiles soon.

--- frontend/forecastical/src/components/MapCard.vue
+++ frontend/forecastical/src/components/MapCard.vue
@@ -6,10 +6,36 @@
 </template>
 
 
+<script>
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
 
-
-
-
+export default {
+  name: 'MapCard',
+  data() {
+    return {
+      map: null, // Declare a variable to hold the map instance
+    };
+  },
+  mounted() {
+    // Initialize the map and assign it to the data property
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
+  },
+};
+</script>
 
 <style scoped>
 .map-card {
@@ -18,7 +44,7 @@
   right: 2%;
   width: 300px; 
   height: 300px; 
-  border: 1px solid #ccc; 
+  border: .5px solid #ccc; 
   border-radius: 8px; 
   overflow: hidden; 
 }
