c4d5245da34400089e820416baf9944284dd2bde
pxd222PranavDhinakar <pxd222@case.edu>
2024-10-23 17:46:28 -0400

    Added navbar and improved component formatting

--- frontend/forecastical/src/App.vue
+++ frontend/forecastical/src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <NavBar />
+    <main class="main-content">
+      <router-view></router-view>
+    </main>
   </div>
 </template>
 
 <script>
+import NavBar from './components/NavBar.vue'  // Note: using relative path
 
 export default {
   name: "App",
+  components: {
+    NavBar
+  }
 };
 </script>
 
@@ -16,13 +23,17 @@ body {
   margin: 0;
   padding: 0;
   background-color: #1e1e1e;
+  min-height: 100vh;
 }
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #ffffff;
+}
+
+.main-content {
+  padding: 20px;
 }
 </style>
