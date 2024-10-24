04f4b114e1d0adcf33799feae1e40cb8494b54f1
ral7ex <98921679+ral7ex@users.noreply.github.com>
2024-10-23 20:00:31 -0400

    Add location search bar to main weather page

--- frontend/forecastical/src/components/searchbar.vue
+++ frontend/forecastical/src/components/searchbar.vue
@@ -9,3 +9,31 @@
   </div>
 </template>
 
+
+<script>
+export default {
+  data() {
+    return {
+      searchQuery: "",
+    };
+  },
+  methods: {
+    onSearch() {
+      this.$emit("search", this.searchQuery);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+input {
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    width: 100%;
+}
+
+
+
+</style>
