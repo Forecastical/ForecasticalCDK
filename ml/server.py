e8f20473b5c4783c966b9f7fb0ec9294864f6c3c
esteban1324 <estebanlinarez@ymail.com>
2024-11-10 14:54:43 -0500

    cv img tagging api call

---
+++ ml/server.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, File, UploadFile, HTTPException, status
+from model_inference import cv_forecast_image
+import uuid
+
+app = FastAPI()
+
+IMAGEDIR = "images/"
+
+
+@app.post("/upload/", status_code=status.HTTP_201_CREATED)
+async def create_upload_file(file: UploadFile = File(...)):
+    if file.content_type != "image/jpeg":
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="only jpg images"
+        )
+
+    file.filename = f"{uuid.uuid4()}.jpg"
+    file_path = f"{IMAGEDIR}{file.filename}"
+   
+    try:
+        contents = await file.read()
+        with open(file_path, "wb") as f:
+            f.write(contents)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="File image upload failed"
+        )
+
+    # predicting the weather based on image 
+    try:
+        prediction = cv_forecast_image(file_path, PATH='./model/vision_model.pth')
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="image prediction failed"
+        )
+
+    return {"prediction": prediction, "filename": file.filename}
+
