477c1fcdcbb080b64044b986d0c4eca72aeb51cf
esteban1324 <estebanlinarez@ymail.com>
2024-11-17 19:35:09 -0500

    comments and Users endpoints are created.

--- backend/server.py
+++ backend/server.py
@@ -6,13 +6,12 @@ import numpy as np
 import pandas as pd
 from pydantic import BaseModel 
 from peewee import *
-
+from typing import Optional
+from datetime import datetime
 
 
 app = FastAPI()
 IMAGEDIR = "images/"
-
-
 db = PostgresqlDatabase('postgres', host='postgres', port=5432, user='postgres', password='postgres')
 
 '''DB objects'''
@@ -74,6 +73,29 @@ class UserCreate(BaseModel):
     use_celsius: bool
     user_alerts: bool
 
+class UserUpdate(BaseModel):
+    username: Optional[str]
+    password: Optional[str]
+    user_fname: Optional[str]
+    user_lname: Optional[str]
+    user_age: Optional[int]
+    home_lat: Optional[float]
+    home_lon: Optional[float]
+    use_celsius: Optional[bool]
+    user_alerts: Optional[bool]
+
+class CreateComment(BaseModel):
+    user_id: int
+    comment: str
+    lat: float
+    lon = float
+
+class EditComment(BaseModel):
+    user_id: Optional[int]
+    comment: Optional[str]
+    lat: Optional[float]
+    lon = Optional[float]
+
 ## Users
 # Register user
 # Login as user ?? 
@@ -93,9 +115,7 @@ class UserCreate(BaseModel):
 
 '''DB API Calls'''
 
-# two API calls with Account class, 1 to create user and other to update user info
-
-# create user with POST request
+"Create New User here"
 @app.post("/users/")
 async def create_user(request = UserCreate):
     # create a user 
@@ -114,9 +134,84 @@ async def create_user(request = UserCreate):
         raise HTTPException(
             status_code=500, detail=f"An unexpected error occurred"
         )
+
+"Update User here"
+@app.patch("/username/{username}")
+async def update_user(user_name: str, user_data: UserUpdate):
+    try:
+        # get user from user_name
+        user = Users.get(Users.username==user_name)
+        # update fields 
+        data_dict = user_data.dict(exclude_unset=True).items()
+        for key, values in data_dict:
+            setattr(user, key, values)
         
+        return {"message": "user updated", "status_code": 200}
+    # user doesn't exist 
+    except Users.DoesNotExist:
+        raise HTTPException(status_code=404, detail="User not found")
+    # internal server error
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="unable to update user"
+        )
 
+"Create comment here"
+@app.post("/comment/")
+async def create_comment(comment: CreateComment):
+    try:
+        new_comment = Comments(comment.user_id, comment.comment, datetime.datetime.now(), 
+                               comment.lon, comment.lat)
+        
+        return {"new comment created": new_comment.comment, "status_code": 201}
+    except IntegrityError as e:
+        raise HTTPException(
+            status_code=400, detail=f"Failed to create comment"
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"An unexpected error occurred"
+        )
 
+"Update User here"
+@app.patch("/username/{user_id}")
+async def update_user(user_id: int, comment: EditComment):
+    try:
+        edit_comment = Comments.get(Comments.user_id==user_id)
+        data_dict = comment.dict(exclude_unset=True).items()
+        
+        for key, value in data_dict:
+            setattr(edit_comment, key, value)
+
+        return {"message": "user updated", "status_code": 200}
+
+    except Comments.DoesNotExist:
+        raise HTTPException(status_code=404, detail="comment not found")
+    # internal server error
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="unable to update comment"
+        )
+
+"Delete comment here"
+@app.delete("/comments/{comment_id}")
+async def update_user(comment_id: int, user_id: int):
+    try:
+        comment = Comments.get_by_id(comment_id)
+
+        if user_id != comment.user_id.id:
+            raise HTTPException(
+                status_code=403, detail="Unable to delete comment"
+            )
+
+        comment.delete_instance()
+
+    except Comments.DoesNotExist:
+        raise HTTPException(status_code=404, detail="Comment not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="An error occurred")
 
 
 '''AI/ML API Calls'''
