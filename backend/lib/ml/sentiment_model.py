b597fb5b2afb3dfae0a3859134ab57988910b6b8
Preston DeLeo <121273342+prestondeleo@users.noreply.github.com>
2024-11-19 19:37:46 -0500

    adding sentiment model implementation

--- backend/lib/ml/sentiment_model.py
+++ backend/lib/ml/sentiment_model.py
@@ -1,4 +1,44 @@
 """
 Sentiment Model for comments
 
+test is IMBD dataset
+
+"""
+
+
+
+import torch
+
+from transformers import pipeline
+
+# Load sentiment analysis pipeline
+sentiment_pipeline = pipeline("sentiment-analysis")
+
+
+# Comments for sentiment analysis
+comments = [
+    "I hate this experience.",
+    "I love this product!",
+    "I hate this experience.",
+    "It's okay, nothing special.",
+    "I hate this experience.",
+    "I hate this experience.",
+    "I hate this experience."
+]
+
+results = sentiment_pipeline(comments)
+sentiment_counts = {"POSITIVE": 0, "NEGATIVE": 0, "NEUTRAL": 0}
+
+for result in results:
+    label = result['label']
+    sentiment_counts[label] += 1
+
+overall_sentiment = max(sentiment_counts, key=sentiment_counts.get)
+print(overall_sentiment)
+
+
+
+
+"""
+Albert or Roberta
 """
